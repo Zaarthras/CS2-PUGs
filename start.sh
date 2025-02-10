166259bf861ac9e26e4bd7830be7af053a911b69
Zaarthras <63236676+Zaarthras@users.noreply.github.com>
2025-02-10 09:32:12 +0100

    Inlined tmux check, added newline at end of file

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@
 
 session="cs2-pug"
 
-tmux has-session -t $session 2>/dev/null
-
-if [ $? != 0 ]; then
+if ! tmux has-session -t $session 2>/dev/null; then
   tmux new -d -s $session
   tmux send-keys -t $session 'source .venv/bin/activate' Enter 'python3 run.py' Enter
   sleep 1s
--- stop.sh
+++ stop.sh
@@ -2,9 +2,7 @@
 
 session="cs2-pug"
 
-tmux has-session -t $session 2>/dev/null
-
-if [ $? != 0 ]; then
+if ! tmux has-session -t $session 2>/dev/null; then
   echo 'Server is already stopped.'
 else
   tmux send-keys -t $session 'deactivate' C-c Enter 'exit' Enter
